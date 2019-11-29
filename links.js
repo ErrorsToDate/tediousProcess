81414d26bb76fff9b407609e98026dfbc2d9d5bc
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2019-11-29 15:27:11 -0500

    newWebsite

---
+++ links.js
@@ -0,0 +1,145 @@
++(function init() {
+  "use strict";
+  const links = [
+    { title: "Play a Game", link: "https://gabrielecirulli.github.io/2048/" },
+    {
+      title: "Final /r/place",
+      link: "https://www.reddit.com/place?webview=true"
+    },
+    { title: "Random XKCD", link: "https://c.xkcd.com/random/comic/" },
+    { title: "Play a Game", link: "https://www.solitr.com/" },
+    {
+      title: "National Geographic - Photo of the Day",
+      link: "http://www.nationalgeographic.com/photography/photo-of-the-day/"
+    },
+    {
+      title: "Road to North Korea",
+      link: "http://www.m1key.me/photography/road_to_north_korea/"
+    },
+    { title: "Building a van!", link: "http://syntheti.cc/van-build/" },
+    {
+      title: "The Little Printf",
+      link: "http://ferd.ca/the-little-printf.html"
+    },
+    {
+      title: "What is equality?",
+      link: "http://thewireless.co.nz/articles/the-pencilsword-on-a-plate"
+    },
+    {
+      title: "Hope the Voyage is a Long One",
+      link: "http://zenpencils.com/comic/131-c-p-cavafy-ithaka/"
+    },
+    {
+      title: "Any computer smart enough to pass the Turing test, would also be smart enough fail it",
+      link: "https://twitter.com/lebenseuche/status/824665931301482496"
+    },
+    {
+      title: "Web Design in 4 Minutes",
+      link: "http://jgthms.com/web-design-in-4-minutes/"
+    },
+    {
+      title: "Throw a paper plane around the world",
+      link: "https://paperplanes.world/"
+    },
+    {
+      title: "There and Back Again",
+      link: "https://twitter.com/elonmusk/status/679137936416329728"
+    },
+    {
+      title: "Log Out, Right Now.",
+      link: "http://theoatmeal.com/comics/log_out"
+    },
+    {
+      title: "You see, I'm just a web server..",
+      link: "https://www-s.acm.illinois.edu/sigarch/projects/sensornode/"
+    },
+    { title: "Got two mins?", link: "http://www.staggeringbeauty.com/" },
+    { title: "How does RGB sound?", link: "http://www.rrrgggbbb.com/" },
+    {
+      title: "A magician/ex-googler decodes how technology is hijacking our daily lives",
+      link: "https://journal.thriveglobal.com/how-technology-hijacks-peoples-minds-from-a-magician-and-google-s-design-ethicist-56d62ef5edf3"
+    },
+    {
+      title: "WAR is a racket. It always has been.",
+      link: "https://www.ratical.org/ratville/CAH/warisaracket.html"
+    },
+    {
+      title: 'You will have to pay for being a "Good Fellow"',
+      link: "https://mikecanex.wordpress.com/2012/12/26/1922-why-i-quit-being-so-accommodating/"
+    },
+    {
+      title: "Start making music, right from your browser.",
+      link: "https://learningmusic.ableton.com/"
+    },
+    {
+      title: "Quick, draw me a sheep!",
+      link: "https://quickdraw.withgoogle.com/"
+    },
+    {
+      title: "Read Calvin & Hobbes",
+      link: "http://marcel-oehler.marcellosendos.ch/comics/ch/"
+    },
+    {
+      title: "Need some motivation?",
+      link: "http://theoatmeal.com/comics/running"
+    },
+    {
+      title: "Fireflies, and what they can teach us about, well, everything.",
+      link: "http://ncase.me/fireflies/"
+    },
+    {
+      title: "The Nearly Mile-Wide Diamond Mine That Helped Build the Soviet Union",
+      link: "http://gizmodo.com/the-nearly-mile-wide-diamond-mine-that-helped-build-the-1593234924"
+    },
+    {
+      title: "How startups such as Dropbox, Airbnb, Groupon and others acquired their first users",
+      link: "https://www.reddit.com/r/Entrepreneur/comments/2clqa3/how_startups_such_as_dropbox_airbnb_groupon_and/"
+    },
+    {
+      title: "URLs are UI",
+      link: "https://www.hanselman.com/blog/URLsAreUI.aspx"
+    },
+    {
+      title: "Is it unethical for me to not tell my employer I’ve automated my job? :D",
+      link: "https://workplace.stackexchange.com/questions/93696/is-it-unethical-for-me-to-not-tell-my-employer-i-ve-automated-my-job"
+    },
+    {
+      title: "Engineers are hired to create business value, not to program things.",
+      link: "http://www.kalzumeus.com/2011/10/28/dont-call-yourself-a-programmer/"
+    },
+    {
+      title: "Posters of some of the classic book quotes",
+      link: "http://imgur.com/a/GNVnD"
+    },
+    {
+      title: "The “Million Dollar Homepage” as a Decaying Digital Artifact",
+      link: "https://lil.law.harvard.edu/blog/2017/07/21/a-million-squandered-the-million-dollar-homepage-as-a-decaying-digital-artifact/"
+    },
+    {
+      title: "The Evolution of Trust",
+      link: "http://ncase.me/trust/"
+    },
+    {
+      title: "Intelligence takes many different forms. It is not one-dimensional. - Career advice from Bill Gates",
+      link: "https://twitter.com/BillGates/status/864100357684609025"
+    },
+    {
+      title: "30-Day Timelapse at Sea",
+      link: "https://www.youtube.com/watch?v=AHrCI9eSJGQ"
+    },
+    {
+      title: "git git git git git",
+      link: "http://caiustheory.com/git-git-git-git-git/"
+    },
+    {
+      title: "Best Time to Visit Any City",
+      link: "https://championtraveler.com/travel-weather-map/"
+    },
+    {
+      title: "Interactive Wind Map of Your City",
+      link: "https://www.windy.com"
+    }
+  ];
+
+  window.link.allLinks = links || [];
+})();
